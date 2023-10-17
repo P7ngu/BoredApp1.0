236ffe5cd901766b0abd79100cdad3ac0c5ae0a6
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-10-17 12:05:58 +0200

    Initial Commit

---
+++ Bored/BoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoredApp.swift
+//  Bored
+//
+//  Created by Matteo Perotta on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct BoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
