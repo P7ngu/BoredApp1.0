14c896bb7754c9d760f65c3b94e0a0d10dbbd1e6
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-10-18 12:10:26 +0200

    challenge model added

--- Bored/ContentView.swift
+++ Bored/ContentView.swift
@@ -11,19 +11,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello,djwidjwi world!")
-            
-            
-            
-            
-            
-            
+        Text("hi")
+        
+        ScrollView{
+            VStack {
+                
+                
+                
+                
+                
+                
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
     
---
+++ Bored/Model/ChallengeModel.swift
@@ -0,0 +1,19 @@
+//
+//  ChallengeModel.swift
+//  Bored
+//
+//  Created by Matteo Perotta on 18/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Challenge: Identifiable{
+    var id: UUID = UUID()
+    var name: String;
+    var content: String;
+    var color: Color = .blue;
+    var completed: Bool = false; //if it has been completed then it is also a memory
+    
+}
