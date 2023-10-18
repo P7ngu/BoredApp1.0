6b8f9f8836b20d72f26ee353d4dfc9b4de67104d
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-10-18 12:03:18 +0200

    fix

--- Bored/ContentView.swift
+++ Bored/ContentView.swift
@@ -5,6 +5,8 @@
 //  Created by Matteo Perotta on 17/10/23.
 //
 
+//This is the Today challenges
+
 import SwiftUI
 
 struct ContentView: View {
@@ -25,10 +27,6 @@ struct ContentView: View {
     }
 }
     
-
-
-var 
-
 
 #Preview {
     ContentView()
