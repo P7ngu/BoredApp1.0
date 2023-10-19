a06de005529950d116da755b261c87b3c717f4fb
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-10-19 11:01:58 +0200

    added challengedetailedview

---
+++ Bored/View/DetailedViews/ChallengeDetailedView.swift
@@ -0,0 +1,39 @@
+//
+//  ChallengeDetailedView.swift
+//  Bored
+//
+//  Created by Matteo Perotta on 19/10/23.
+//
+
+import SwiftUI
+
+struct ChallengeDetailedView: View {
+    var viewmodel = ChallengeViewModel();
+    
+    var challenge = Challenge(name: "Test challenge", content: "Test content", completed: true)
+    
+    var body: some View {
+        ZStack{ //to change the background color
+            // .blue.ignoresSafeArea().opacity(0.5)
+            
+            
+            VStack{
+                Image(challenge.imageName).resizable().aspectRatio(contentMode: .fit)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    .padding()
+                
+                Text(challenge.name + ": ")
+                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    .bold()
+                
+                Text(challenge.content)
+                    .font(.callout)
+            }
+        }
+    }
+}
+
+#Preview {
+    ChallengeDetailedView()
+}
