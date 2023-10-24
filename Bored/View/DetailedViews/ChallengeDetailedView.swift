0a7875cd73ebd2c5cdba566f5129580983ff46ca
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-10-24 12:12:26 +0200

    added canera button

--- Bored/View/DetailedViews/ChallengeDetailedView.swift
+++ Bored/View/DetailedViews/ChallengeDetailedView.swift
@@ -16,47 +16,55 @@ struct ChallengeDetailedView: View {
     @State var selectedImage: UIImage = UIImage()
     
     var body: some View {
-        ZStack{ //to change the background color
-            // .blue.ignoresSafeArea().opacity(0.5)
-            
-            
-            VStack{
-                Text(challenge.name + ": ")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    .bold()
-                ZStack{
-                    
-                    Image(challenge.imageName).resizable().aspectRatio(contentMode: .fit)
-                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-                        .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
-                        .padding()
-                    if (selectedImage != nil){
-                        Image (uiImage: selectedImage)
-                            .resizable()
-                            .frame(width: 200, height: 200)
+        NavigationView{
+            ZStack{ //to change the background color
+                // .blue.ignoresSafeArea().opacity(0.5)
+                
+                
+                VStack{
+                    //Text(challenge.name + ": ")
+                     //   .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                     //   .bold()
+                    ZStack{
+                        
+                        Image(challenge.imageName).resizable().aspectRatio(contentMode: .fit)
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
                             .padding()
+                        if (selectedImage != nil){
+                            Image (uiImage: selectedImage)
+                                .resizable()
+                                .frame(width: 200, height: 200)
+                                .padding()
+                        }
                     }
-                }
-
-                Text(challenge.content)
-                    .font(.callout).padding()
-                
-                Button{
-                    isPickerShowing = true
-                } label: {
-                    Text("Select a Photo")
-                }
-                .sheet(isPresented: $isPickerShowing, onDismiss: nil) {
                     
-                    ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+                    Text(challenge.content)
+                        .font(.callout).padding()
                     
-                }.padding(20)
-                
-                Text("Your notes: ")
-                    .font(.title3)
-                TextEditor(text: .constant("This activity helped me connect with my mind!"))
-                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
-                    .foregroundColor(.gray).padding(10)
+                  
+                    
+                    Text("Your notes: ")
+                        .font(.title3)
+                    TextEditor(text: .constant("This activity helped me connect with my mind!"))
+                        .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                        .background(.gray)
+                        .foregroundColor(.gray).padding(10)
+                        
+                }.navigationTitle(challenge.name)
+                .toolbar{
+                    Button{
+                        isPickerShowing = true
+                    } label: {
+                        Text("")
+                        Image(systemName: "photo.badge.plus")
+                    }
+                    .sheet(isPresented: $isPickerShowing, onDismiss: nil) {
+                        
+                        ImagePicker(selectedImage: $selectedImage, isPickerShowing: $isPickerShowing)
+                        
+                    }.padding(20)
+                }
             }
         }
     }
