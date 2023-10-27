a0efe39b8f4ea2bb15ffa8df9c1fba1f14a4f945
SpacebornYu <120189548+SpacebornYu@users.noreply.github.com>
2023-10-27 12:26:27 +0200

    .

--- Bored/View/ContentView.swift
+++ Bored/View/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
     @State var selection: String = "Unaccepted"
     let filterOptions: [String] = ["Unaccepted", "Accepted"]
     
- 
+    
     
     
     var body: some View {
@@ -48,77 +48,76 @@ struct ContentView: View {
                 ScrollView{
                     ZStack{
                         //Color.yellow.ignoresSafeArea().opacity(0.3)
-                    VStack {
-                        ZStack{//Color.yellow.opacity(0.2).ignoresSafeArea()
-                              //  .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
-                            VStack(alignment: .leading){
-                                NavigationLink(destination: ChallengeDetailedView(modelContext: modelContext, challenge: currentChallenge)){
-                                    GroupBox{
-                                        VStack{
-                                            Text(currentChallenge.name).bold().font(.title)
-                                                .foregroundStyle(Color.orange)
-                                            Image(currentChallenge.imageName)
-                                                .resizable()
-                                                .dynamicTypeSize(.medium)
-                                                .aspectRatio(contentMode: .fit)
-                                                .clipShape(Rectangle())
-                                                .shadow(radius: 10)
-                                                .opacity(0.5)
-                                                .padding(10)
-                                                .frame(width: 300, height: 300)
-                                            Text("For this challenge you need to...").font(.title2).foregroundStyle(.black)
-                                            HStack{
-                                                Text("show details").font(.title2)
-                                                Image(systemName: "info.bubble")
+                        VStack {
+                            ZStack{//Color.yellow.opacity(0.2).ignoresSafeArea()
+                                //  .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                                VStack(alignment: .leading){
+                                    NavigationLink(destination: ChallengeDetailedView(modelContext: modelContext, challenge: currentChallenge)){
+                                        GroupBox{
+                                            VStack{
+                                                Text(currentChallenge.name).bold().font(.title)
+                                                    .foregroundStyle(Color.orange)
+                                                Image(currentChallenge.imageName)
+                                                    .resizable()
+                                                    .dynamicTypeSize(.medium)
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .clipShape(Rectangle())
+                                                    .shadow(radius: 10)
+                                                    .opacity(0.5)
+                                                    .padding(10)
+                                                    .frame(width: 300, height: 300)
+                                                Text("For this challenge you need to...").font(.title2).foregroundStyle(.black)
+                                                HStack{
+                                                    Text("show details").font(.title2)
+                                                    Image(systemName: "info.bubble")
+                                                }
                                             }
-                                        }
-                                        
-                                    }.foregroundStyle(Color.blue)
+                                            
+                                        }.foregroundStyle(Color.blue)
+                                    }
+                                    
                                 }
-                                
-                            }
-                }.navigationTitle("Today's Challenge is:")
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                    .tabItem {
-                        Label("Today", systemImage: "cloud.sun")
+                            }.navigationTitle("Today's Challenge is:")
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                            
+                                .tabItem {
+                                    Label("Today", systemImage: "cloud.sun")
+                                }
+                            MemoriesView()
+                                .tabItem{
+                                    Label ("Memories", systemImage: "memories")
+                                }
+                            AchievementsView()
+                                .tabItem{
+                                    Label("Achievements", systemImage: "trophy.fill")
+                                }
+                        } //end of tabview
                     }
-                MemoriesView()
-                    .tabItem{
-                        Label ("Memories", systemImage: "memories")
-                    }
-                AchievementsView()
-                    .tabItem{
-                        Label("Achievements", systemImage: "trophy.fill")
-                    }
-            } //end of tabview
-        }
-    }
-    
-    #Preview {
-        ContentView()
-=======
+                }
+                
+                #Preview {
+                    ContentView()
                 }.background {
                     //RoundedRectangle(cornerRadius: 40)
-                       // .foregroundColor(.white)
-                       // .opacity(0.5)
-                     //   .blur(radius: /*@START_MENU_TOKEN@*/11.0/*@END_MENU_TOKEN@*/)
-                       // .scenePadding(/*@START_MENU_TOKEN@*/.minimum/*@END_MENU_TOKEN@*/, edges: /*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    // .foregroundColor(.white)
+                    // .opacity(0.5)
+                    //   .blur(radius: /*@START_MENU_TOKEN@*/11.0/*@END_MENU_TOKEN@*/)
+                    // .scenePadding(/*@START_MENU_TOKEN@*/.minimum/*@END_MENU_TOKEN@*/, edges: /*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     //RoundedRectangle(cornerRadius: 30)
-                     //   .strokeBorder(.yellow, lineWidth: 10)
-                       // .frame(height: )
+                    //   .strokeBorder(.yellow, lineWidth: 10)
+                    // .frame(height: )
                 }
                 .navigationTitle("Today's Challenge")
-               // .foregroundStyle(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                // .foregroundStyle(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 
             }
             
@@ -134,25 +133,20 @@ struct ContentView: View {
                     Label("Achievements", systemImage: "trophy.fill")
                 }
         } //end of tabview
->>>>>>> main
-    }
-    
-}
-<<<<<<< HEAD
-=======
-
-/*#Preview {
-   do {
-        let config = ModelConfiguration(isStoredInMemoryOnly: true)
-      let container = try ModelContainer(for: Challenge.self, configurations: config)
         
-        let example = Challenge(id: UUID(), name: "mkrmf", content: "diejd", completed: false, imageName: "noimafe", assignedDate: "00")
-        return ContentView()
-            .modelContainer(container)
-    } catch {
-        fatalError("Failed to create model container.")
+        /*#Preview {
+         do {
+         let config = ModelConfiguration(isStoredInMemoryOnly: true)
+         let container = try ModelContainer(for: Challenge.self, configurations: config)
+         
+         let example = Challenge(id: UUID(), name: "mkrmf", content: "diejd", completed: false, imageName: "noimafe", assignedDate: "00")
+         return ContentView()
+         .modelContainer(container)
+         } catch {
+         fatalError("Failed to create model container.")
+         }
+         }
+         */
+        
     }
 }
- */
-
->>>>>>> main
