faa0c8d72310513caef295983937a94d294c100d
P7ngu <60442992+P7ngu@users.noreply.github.com>
2023-11-01 16:24:47 +0100

    test 13 pro max

--- Bored/View/DetailedViews/ChallengeDetailedView.swift
+++ Bored/View/DetailedViews/ChallengeDetailedView.swift
@@ -61,28 +61,28 @@ struct ChallengeDetailedView: View {
                                         if(chall.completed == false){
                                             GroupBox(label:
                                                         Label("Your notes:", systemImage: "pencil.line")
-                                            ){ TextField("Notes", text: $userNotes, prompt: Text("Please input your notes"), axis: .vertical)
-                                                    .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
-                                                    .foregroundStyle(.gray)
-                                                    .foregroundColor(Color.gray)
-                                                    .focused($isFocused)
+                                            ){ 
+                                                VStack{
+                                                    Button("Done") {
+                                                        let notes = userNotes
+                                                        
+                                                        MemoryViewModel(challenges: challenges).convertChallengeIntoMemory(challenge: challenge, context: modelContext, notes: notes)
+                                                        
+                                                        // viewmodel.completedChallengeCounter += 1 //not persistent
+                                                        showingConfirmation = true
+                                                        chall.completed = true
+                                                        challenge.completed = true
+                                                        
+                                                    }    
+                                                    TextField("Notes", text: $userNotes, prompt: Text("Please input your notes"), axis: .vertical)
+                                                        .frame(height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, alignment: .center)
+                                                        .foregroundStyle(.gray)
+                                                        .foregroundColor(Color.gray)
+                                                        .focused($isFocused)
+                                                        .padding(10)
                                                     
-                                                
-                                                Button("Done") {
-                                                    let notes = userNotes
                                                     
-                                                    MemoryViewModel(challenges: challenges).convertChallengeIntoMemory(challenge: challenge, context: modelContext, notes: notes)
-                                                    
-                                                    // viewmodel.completedChallengeCounter += 1 //not persistent
-                                                    showingConfirmation = true
-                                                    chall.completed = true
-                                                    challenge.completed = true
-                                                    
-                                                }.alert("Notes saved!", isPresented: $showingConfirmation) {
-                                                    Button("OK", role: .cancel) {
-                                                        showingConfirmation = false
-                                                    }
-                                                }
+                                                                                           }
                                                 
                                             }
                                         }else { // the challenge has already been completed, you can't edit the text anymore.
